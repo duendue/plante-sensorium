6506f41d2e505ffb1df644edf01e0704039a504d
duendue <duendue@hotmail.dk>
2023-03-17 00:32:17 +0100

    added updated sketches

--- Simpel_Sensor/Simpel_Sensor.ino
+++ Simpel_Sensor/Simpel_Sensor.ino
@@ -2,19 +2,28 @@
 
 CapacitiveSensor cs_7_8 = CapacitiveSensor(7,8); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
 unsigned long csSum;
+char val;
 
 void setup() {
-    Serial.begin(9600);
-  
+    Serial.begin(115200);
+
+    //establishContact();
 }
 
+void establishContact(){
+  while (Serial.available() <= 0){
+    Serial.println("A");
+    delay(300);
+  }
+}
 void loop() {
-    //Serial.println("serial working");
-    CSread();
+    //Serial.println("serial working")
+  CSread();
 }
 
 void CSread() {
     long cs = cs_7_8.capacitiveSensor(80); //a: Sensor resolution is set to 80
-    Serial.print("Plant_A/");
-    Serial.println(cs);
+
+    Serial.println("Plant_A");
+    Serial.println(cs);    
 }
