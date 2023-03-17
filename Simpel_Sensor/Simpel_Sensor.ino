4fe31b43cdcb5ad0f751f6d3b31ecdd47ff6ff59
duendue <duendue@hotmail.dk>
2023-03-17 14:31:41 +0100

    updated arduino sketch to accomodate multiple sensors (when edited)

--- Simpel_Sensor/Simpel_Sensor.ino
+++ Simpel_Sensor/Simpel_Sensor.ino
@@ -1,8 +1,12 @@
 #include <CapacitiveSensor.h>
 
-CapacitiveSensor cs_7_8 = CapacitiveSensor(7,8); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
-CapacitiveSensor cs_9_10 = CapacitiveSensor(9,10); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
-unsigned long csSum;
+CapacitiveSensor cs_2_3 = CapacitiveSensor(2,3); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
+CapacitiveSensor cs_4_5 = CapacitiveSensor(4,5); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
+CapacitiveSensor cs_6_7 = CapacitiveSensor(6,7); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
+/*CapacitiveSensor cs_8_9 = CapacitiveSensor(8,9); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
+CapacitiveSensor cs_10_11 = CapacitiveSensor(10,11); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
+CapacitiveSensor cs_12_13 = CapacitiveSensor(12,13); //10M Resistor between pins 7 and 8, you may also connect an antenna on pin 8
+*/
 char val;
 
 void setup() {
@@ -19,15 +23,18 @@ void establishContact(){
   }
 }
 void loop() {
-    //Serial.println("serial working")
+  
   CSread();
-  //countSensors();
 }
 
 void CSread() {
-    long cs0 = cs_7_8.capacitiveSensor(80); //a: Sensor resolution is set to 80
-    long cs1 = cs_9_10.capacitiveSensor(80); //a: Sensor resolution is set to 80
-
+  long cs0 = cs_2_3.capacitiveSensor(80); //a: Sensor resolution is set to 80
+  long cs1 = cs_4_5.capacitiveSensor(80); //a: Sensor resolution is set to 80
+  //long cs2 = cs_6_7.capacitiveSensor(80); //a: Sensor resolution is set to 80
+ /* long cs3 = cs_8_9.capacitiveSensor(80); //a: Sensor resolution is set to 80
+  long cs4 = cs_10_11.capacitiveSensor(80); //a: Sensor resolution is set to 80
+  long cs5 = cs_12_13.capacitiveSensor(80); //a: Sensor resolution is set to 80
+*/
     if(cs0 > 0){
       Serial.print("Plant_A/");
       Serial.print(cs0);
@@ -35,6 +42,27 @@ void CSread() {
     }
     if(cs1 > 0){
       Serial.print("Plant_B/");
-      Serial.println(cs1);     
+      Serial.print(cs1);
+      //Serial.print("/");       
     }
+  /*  if(cs2 > 0){
+      Serial.print("Plant_C/");
+      Serial.print(cs2);
+      Serial.print("/");       
+    }
+   /* if(cs3 > 0){
+      Serial.print("Plant_D/");
+      Serial.print(cs3);
+      Serial.print("/");       
+    }
+    if(cs4 > 0){
+      Serial.print("Plant_E/");
+      Serial.print(cs4);
+      Serial.print("/");       
+    }
+    if(cs5> 0){
+      Serial.print("Plant_F/");
+      Serial.print(cs5);      
+    }*/
+      Serial.println();
 }
